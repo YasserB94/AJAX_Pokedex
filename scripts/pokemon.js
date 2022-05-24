1fa6d5f85b13b5b7e777b080141afd45233ecd4b
Yasser <yasserbal@icloud.com>
2022-05-24 16:45:08 +0200

    Replaced DOMCODE with getters for Pokedex

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -6,47 +6,66 @@ export default class Pokemon {
         this.sprite = sprite;
         this.prevolution = prevolutions;
         this.evolution = evolutions;
-        this.init();
+        // this.init();
     }
-    init(){
-        this.createDOMElements();
-        this.setDOMElements();
-        this.setDOMStyles();
+    // init(){
+    // }
+    getID(){
+        return this.id;
     }
-    createDOMElements(){
-        this.DOMpokemonContainer = document.createElement('div');
-        this.DOMpokemonName = document.createElement('p')
-        this.DOMpokemonMoves = document.createElement('p')
-        this.DOMpokemonSprite = document.createElement('img')
-        this.DOMpokemonPrevolutions = document.createElement('p')
-        this.DOMpokemonEvolutions = document.createElement('p')
+    getName(){
+        return this.name;
     }
-    setDOMElements(){
-        this.DOMpokemonName.innerText = this.name;
-        this.DOMpokemonName.setAttribute('id','pokemon-name')
-        this.DOMpokemonMoves.innerText = this.moves;
-        this.DOMpokemonMoves.setAttribute('id','pokemon-moves')
-        this.DOMpokemonSprite.src = this.sprite;
-        this.DOMpokemonSprite.setAttribute('id','pokemon-sprite')
-        this.DOMpokemonSprite.alt = `An image representing: ${this.name}`;
-        this.DOMpokemonPrevolutions.innerText = this.prevolution;
-        this.DOMpokemonPrevolutions.setAttribute('id','pokemon-preevolutions')
-        this.DOMpokemonEvolutions.innerText = this.evolution;
-        this.DOMpokemonEvolutions.setAttribute('id','pokemon-evolutions')
-        this.DOMpokemonContainer.appendChild(this.DOMpokemonSprite);
-        this.DOMpokemonContainer.appendChild(this.DOMpokemonName);
-        this.DOMpokemonContainer.appendChild(this.DOMpokemonMoves);
-        this.DOMpokemonContainer.appendChild(this.DOMpokemonPrevolutions)
-        this.DOMpokemonContainer.appendChild(this.DOMpokemonEvolutions)
+    getMoves(){
+        return this.moves;
     }
-    setDOMStyles(){
-        this.DOMpokemonContainer.style.display = 'flex';
-        this.DOMpokemonContainer.style.flexDirection = 'column';
-        this.DOMpokemonContainer.style.backgroundColor = 'pink';
-        this.DOMpokemonContainer.style.textAlign = 'center';
+    getSprite(){
+        return this.sprite;
     }
-    getDOMElements(){
-        return this.DOMpokemonContainer;
+    getPrevolution(){
+        return this.prevolution;
     }
+    getEvolution(){
+        return this.evolution;
+    }
+
+
+
+
+    // createDOMElements(){
+    //     this.DOMpokemonContainer = document.createElement('div');
+    //     this.DOMpokemonName = document.createElement('p')
+    //     this.DOMpokemonMoves = document.createElement('p')
+    //     this.DOMpokemonSprite = document.createElement('img')
+    //     this.DOMpokemonPrevolutions = document.createElement('p')
+    //     this.DOMpokemonEvolutions = document.createElement('p')
+    // }
+    // setDOMElements(){
+    //     this.DOMpokemonName.innerText = this.name;
+    //     this.DOMpokemonName.setAttribute('id','pokemon-name')
+    //     this.DOMpokemonMoves.innerText = this.moves;
+    //     this.DOMpokemonMoves.setAttribute('id','pokemon-moves')
+    //     this.DOMpokemonSprite.src = this.sprite;
+    //     this.DOMpokemonSprite.setAttribute('id','pokemon-sprite')
+    //     this.DOMpokemonSprite.alt = `An image representing: ${this.name}`;
+    //     this.DOMpokemonPrevolutions.innerText = this.prevolution;
+    //     this.DOMpokemonPrevolutions.setAttribute('id','pokemon-preevolutions')
+    //     this.DOMpokemonEvolutions.innerText = this.evolution;
+    //     this.DOMpokemonEvolutions.setAttribute('id','pokemon-evolutions')
+    //     this.DOMpokemonContainer.appendChild(this.DOMpokemonSprite);
+    //     this.DOMpokemonContainer.appendChild(this.DOMpokemonName);
+    //     this.DOMpokemonContainer.appendChild(this.DOMpokemonMoves);
+    //     this.DOMpokemonContainer.appendChild(this.DOMpokemonPrevolutions)
+    //     this.DOMpokemonContainer.appendChild(this.DOMpokemonEvolutions)
+    // }
+    // setDOMStyles(){
+    //     this.DOMpokemonContainer.style.display = 'flex';
+    //     this.DOMpokemonContainer.style.flexDirection = 'column';
+    //     this.DOMpokemonContainer.style.backgroundColor = 'pink';
+    //     this.DOMpokemonContainer.style.textAlign = 'center';
+    // }
+    // getDOMElements(){
+    //     return this.DOMpokemonContainer;
+    // }
 }
 
